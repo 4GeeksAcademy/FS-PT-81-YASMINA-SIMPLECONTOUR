dbd488faefc908906c6978fdd7b82f1ee1180425
Yabel90 <yasminabelhaj@hotmail.com>
2024-11-04 23:21:22 +0000

    chore somple contour final

---
+++ src/js/component/card.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Card = ({ Digit }) => {
+    return (
+        <div className=" counter Card ">
+            {Digit}
+        </div>
+    )
+}
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { Card } from "./card";
+import { LuClock9 } from "react-icons/lu";
+
 
 //create your first component
-const Home = () => {
+const Home = ({ time }) => {
+	let simpletime = time.toString().split('').reverse()
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className=" wrapper d-flex justify-content-center align-items-center p-2 bg-dark gap-1  " >
+			    <Card Digit={<LuClock9 />} />
+				<Card Digit={Math.floor(time / 100000) % 100000} />
+				<Card Digit={Math.floor(time / 10000) % 10000} />
+				<Card Digit={Math.floor(time / 1000) % 1000} />
+				<Card Digit={Math.floor(time / 100) % 100} />
+				<Card Digit={Math.floor(time / 10) % 10} />
+				<Card Digit={Math.floor(time % 10)} />
+
+			</div>
+		</>
 	);
 };
-
 export default Home;
